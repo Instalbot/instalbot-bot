32f2610e558e28e6eaadf1eedd1e4b67fb10d3d4
Nimplex <39964594+Nimplex@users.noreply.github.com>
2024-01-12 08:35:11 +0000

    new changes

--- main3.py
+++ main3.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 from random import randint
 from dotenv import load_dotenv
-from sqlalchemy import text
+from sqlalchemy import text, update
 import pytz
 import threading
+import time
 
 load_dotenv()
 
@@ -11,12 +12,25 @@ from db import db, models
 
 botQueue = dict([])
 
+def spawn_bot(bot):
+    with db.Session() as session:
+        try:
+            changes = update(models.Flag).where(models.Flag.userid == bot.userid).values(todo=False)
+            session.execute(changes)
+            session.commit()
+        except Exception as e:
+            session.rollback()
+            return
+
+    print(bot.to_dict())
+
+
 def refresh_queue():
     with db.Session() as session:
         # Retrieve all flags that are in range of flag's hoursrange
         result = session.query(models.Flag).filter(
             text("numeric_add(EXTRACT(HOUR FROM now()), EXTRACT(MINUTE FROM now()) / 60) <@ hoursrange")
-        ).all()
+        ).filter_by(todo=True).all()
         
         # Insert flags to botQueue
         keys = botQueue.keys()
@@ -31,12 +45,29 @@ def refresh_queue():
             else:
                 pass
 
-print(botQueue)
 
 def schedule_refresh():
-    threading.Timer(3600, schedule_refresh).start()
+    timer = threading.Timer(1, schedule_refresh)
+    timer.start()
     refresh_queue()
 
-schedule_refresh()
 
-print(botQueue)
+def start_waiter():
+    while True:
+        now = datetime.now(pytz.timezone('Europe/Warsaw'))
+        timestamp = round(now.timestamp()) - now.minute * 60
+        for bot in botQueue.copy():
+            if botQueue[bot][1] > timestamp:
+                thread = threading.Thread(target=spawn_bot, args=(botQueue[bot][0],))
+                thread.start()
+                del botQueue[bot]
+            else:
+                pass
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    refreshing = threading.Thread(target=schedule_refresh)
+    refreshing.start()
+    waiter = threading.Thread(target=start_waiter)
+    waiter.start()
