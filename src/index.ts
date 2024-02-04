086babd257f6a4b28fcf38a38527bff12b75107c
Nimplex <39964594+Nimplex@users.noreply.github.com>
2024-02-04 13:08:11 +0100

    implemented whole session logic

--- src/index.ts
+++ src/index.ts
@@ -42,7 +42,19 @@ function xorEncryption(text: string, key: string): string {
     }
 
     return encryptedText;
-}
+};
+
+async function sleep(timeout: number) {
+    return new Promise((resolve, _) => {
+        setTimeout(() => {
+            resolve(true);
+        }, timeout);
+    });
+};
+
+function random(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min) + min);
+};
 
 async function startBot(userId: number) {
     if (!process.env.INSTALING_KEY)
@@ -61,7 +73,7 @@ async function startBot(userId: number) {
     let res;
 
     try {
-        res = await client.query("SELECT * FROM users, words, flags WHERE users.userid = $1", [userId]);
+        res = await client.query("SELECT * FROM users INNER JOIN flags on users.userid = flags.userid INNER JOIN words on users.userid = words.userid WHERE users.userid = $1", [userId]);
     } catch(err) {
         return logger.error(`startBot(): Cannot query database: ${(err as Error).message}`);
     }
@@ -82,26 +94,35 @@ async function startBot(userId: number) {
             headless: false
         });
     } catch(err) {
-        return logger.error(`startBot(): Cannot spawn browser: ${(err as Error).message}`)
+        return logger.error(`startBot(): Cannot spawn browser: ${(err as Error).message}`);
     }
 
     const context = await browser.newContext({
-        ...devices["Desktop Chrome"]
+        ...devices["Desktop Chrome"],
     });
+
+    context.setDefaultTimeout(2000);
+
     const page = await context.newPage();
 
     await page.goto("https://instaling.pl/teacher.php?page=login");
 
     await page.waitForLoadState("domcontentloaded");
 
+    await sleep(random(300, 1000));
+
     await page.locator("xpath=/html/body/div[2]/div[2]/div[1]/div[2]/div[2]/button[1]")
-        .click({ timeout: 5000 })
+        .click()
         .catch(() => logger.warn(`startBot(): Cannot find cookie button for session ${userId}`));
 
+    await sleep(random(300, 1000));
+
     try {
-        await page.locator('//*[@id="log_email"]').fill(userData.instaling_user);
-        await page.locator('//*[@id="log_password"]').fill(password);
-        await page.locator('//*[@id="main-container"]/div[3]/form/div/div[3]/button').click({ timeout: 2000 });
+        await page.locator('//*[@id="log_email"]').pressSequentially(userData.instaling_user, { timeout: 20000, delay: random(250, 500) });
+        await sleep(random(500, 1000));
+        await page.locator('//*[@id="log_password"]').pressSequentially(password, { timeout: 20000, delay: random(230, 600) });
+        await sleep(random(500, 1500));
+        await page.locator('//*[@id="main-container"]/div[3]/form/div/div[3]/button').click();
     } catch(err) {
         await context.close();
         await browser.close();
@@ -118,14 +139,16 @@ async function startBot(userId: number) {
 
     // --[ SESSION LOGIC ]------------------------------------------------------
 
+    await sleep(random(600, 1200));
+
     try {
-        await page.locator('//*[@id="student_panel"]/p[1]/a').click({ timeout: 2000 });
+        await page.locator('//*[@id="student_panel"]/p[1]/a').click();
     } catch(err) {
         logger.warn(`startBot(): Primary student panel method failed, using backup: ${(err as Error).message}`);
 
         const r = await page.getByRole("link")
             .filter({ hasText: /Dokończ sesję|Zacznij codzienną sesję/ })
-            .click({ timeout: 2000 })
+            .click()
             .catch(async err => {
                 await context.close();
                 await browser.close();
@@ -138,11 +161,13 @@ async function startBot(userId: number) {
 
     await page.waitForLoadState("domcontentloaded");
 
+    await sleep(random(500, 2000));
+
     try {
-        await page.locator('//*[@id="start_sesion_button"]').click({ timeout: 2000 })
+        await page.locator('//*[@id="start_session_button"]').click();
     } catch(errP) {
         const r = await page.locator('//*[@id="continue_session_button"]')
-            .click({ timeout: 2000 })
+            .click()
             .catch(async err => {
                 await context.close();
                 await browser.close();
@@ -152,8 +177,67 @@ async function startBot(userId: number) {
             return;
     }
 
-    // await context.close();
-    // await browser.close();
+    await page.waitForLoadState("domcontentloaded");
+
+    const truthTable: {
+        [key: string]: string
+    } = {};
+
+    userData.list.forEach((x) => (truthTable[x.key] = x.value));
+
+    while (true) {
+        await sleep(random(500, 1000));
+        await page.waitForLoadState("domcontentloaded");
+
+        try {
+            const isNew = await page.locator("xpath=/html/body/div/div[8]/div[3]/div[1]").isVisible();
+            if (isNew) {
+                await page.locator('//*[@id="dont_know_new"]').click();
+                continue;
+            }
+
+            const possibleWord = await page.locator('//*[@id="possible_word_page"]').isVisible();
+            if (possibleWord) {
+                await page.locator('//*[@id="skip"]').click();
+                continue;
+            }
+
+            const finishPage = await page.locator('//*[@id="finish_page"]').isVisible();
+            if (finishPage) {
+                logger.log(`startBot(): Finished session ${userId}`);
+                break;
+            }
+        } catch(err) {
+            logger.error(`startBot(): Cannot handle "Czy znasz już to słówko?": ${(err as Error).message}`)
+        }
+
+        const word = await page.locator('//*[@id="question"]/div[2]/div[2]').innerHTML();
+        const translation = truthTable[word];
+
+        if (!translation)
+            break;
+
+        try {
+            await page.locator('//*[@id="answer"]').pressSequentially(translation, { timeout: 60000, delay: random(230, 400) });
+            await page.locator('//*[@id="check"]').click();
+        } catch(err) {
+            logger.error(`startBot(): Couldn't fill or skip word "${translation} (${word})" for session ${userId}`);
+            continue;
+        }
+
+        await sleep(random(500, 1000));
+        await page.waitForLoadState("domcontentloaded");
+
+        try {
+            await page.locator('//*[@id="next_word"]', { hasText: "Następne" }).click();
+        } catch(err) {
+            logger.error(`Cannot press "next_word" for session ${userId}`);
+            break;
+        }
+    }
+
+    await context.close();
+    await browser.close();
 }
 
 for(let i = 1; i <= 3; i++) {
