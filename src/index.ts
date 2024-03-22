a7d757cf135808cc7a63984bd92def8a7410c469
Kaczka <80482933+Kaczka212@users.noreply.github.com>
2024-03-22 22:36:00 +0100

    ok

--- src/index.ts
+++ src/index.ts
@@ -346,16 +346,14 @@ async function worker() {
         
             logger.log(`worker(): Received a task, starting bot for user ${userId}`);
         
-            channel.ack(msg);
-
             const context = await browser.newContext({
                 ...devices["Desktop Chrome"],
             });
-        
+            
             context.setDefaultTimeout(60000);
-        
+            
             const [res, err] = await startBot(userId, context);
-        
+            
             context.close();
 
             switch (res) {
@@ -391,6 +389,8 @@ async function worker() {
             /*channel.sendToQueue(msg.properties.replyTo, Buffer.from(res.toString()),{
                 correlationId: msg.properties.correlationId
             });*/
+
+            channel.ack(msg);
         });
 
     } catch (error) {
